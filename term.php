bf4c472413cb2a249d6739785742722aadf192f7
Leander Pfeiffer <32580513+pfeifferleander@users.noreply.github.com>
2017-10-14 21:17:28 +0000

    Timer added, styling not optimal

--- term.php
+++ term.php
@@ -1,31 +1,43 @@
 
 			<?php
 				session_start();
-				$fp = fopen("/sys/devices/w1_bus_master1/28-051670b0a3ff/w1_slave","r");
-				if($fp){
-					$zeile = fread($fp, 200); 
-					$termStr = substr($zeile,-6);
-					$term = floatval($termStr);
-					$term = $term / 1000;
-					$bufferStr = $_SESSION["buffer"];
-					$tempStr = $_SESSION["temperature"];
-					echo "<p><strong>$term °C</strong></p>";
-					echo "<aside>Die Richttemperatur beträgt: $tempStr °C<br>Der Buffer beträgt: $bufferStr °C</aside>";
-					$temp = floatval($tempStr);
-					$buffer = floatval($bufferStr); 
-					if($term < $temp - $buffer){
-						echo "<p class=\"hot\">Es ist zu kalt!</p>";
-					}
-					elseif($term > $temp + $buffer){
-						echo "<p class=\"hot\">Es ist zu heiß!</p>";
+				$time = $_SESSION["time"];
+				if($time>0){
+					$minTime = floor($time / 60);
+					$sekTime = $time % 60;
+					$sekStr = strval($sekTime);
+					$minStr = strval($minTime);
+					if(strlen($sekStr)==1){
+						$sekStr = "0" . $sekStr;	
+						}
+					$timeStr = $minStr . " : " . $sekStr . " min";
+					echo "<div class=\"timer\"><p>$timeStr</p></div>";
+					$_SESSION["time"] = $time -1;
 						
+					$fp = fopen("/sys/devices/w1_bus_master1/28-051670b0a3ff/w1_slave","r");
+					if($fp){
+						$zeile = fread($fp, 200); 
+						$termStr = substr($zeile,-6);
+						$term = floatval($termStr);
+						$term = $term / 1000;
+						$bufferStr = $_SESSION["buffer"];
+						$tempStr = $_SESSION["temperature"];
+						echo "<p id=\"warning\"><strong>$term °C</strong></p>";
+						//echo "<hr><p>Die Richttemperatur beträgt: $tempStr °C<br>Der Buffer beträgt: $bufferStr °C</p>";
+						$temp = floatval($tempStr);
+						$buffer = floatval($bufferStr); 
+						if($term < $temp - $buffer){
+							echo "<p class=\"hot\" id=\"warning\">Es ist zu kalt!</p>";
+						}elseif($term > $temp + $buffer){
+							echo "<p class=\"hot\" id=\"warning\">Es ist zu heiß!</p>";
+						}else{
+							echo "<p>Alles gut</p>";}
+						fclose($fp);
+						echo "<p>Die Richttemperatur beträgt: $tempStr °C<br>Der Buffer beträgt: $bufferStr °C</p>";
+					}else{
+						echo "Data not found";}
+				}else{
+					echo "<p>Schritt beendet!</p>";
 					}
-					else{
-						echo "Alles gut";
-					}
-					fclose($fp);
-				}
-				else
-					echo "Data not found";
 		?>
 		<br>
