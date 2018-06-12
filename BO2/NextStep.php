1b84ad3b987ea8eb134f6ef317c39b49a08e93e4
Leander Pfeiffer <32580513+pfeifferleander@users.noreply.github.com>
2018-06-12 19:22:01 +0200

    Update NextStep.php

--- BO2/NextStep.php
+++ BO2/NextStep.php
@@ -3,5 +3,9 @@ session_start();
 
 $aktuellerSchritt = $_SESSION["aktuellerSchritt"];
 $_SESSION["aktuellerSchritt"] = $aktuellerSchritt + 1;
+//Der aktuelle schritt wird um eins erhöht
+
 $_SESSION["buttonPressed"] = 1;
+//Vermerk das der Button gedrückt wurde
+
 ?>
